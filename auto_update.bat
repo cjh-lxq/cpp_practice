ca049db424ad0c86cd2295bd0e853b29b65e891d
cjh-lxq <18601277500@163.com>
2018-11-28 22:35:36 +0800

    2018-11-28-22:35:36

---
+++ auto_update.bat
@@ -0,0 +1,8 @@
+set filename=%date:~0,4%-%date:~5,2%-%date:~8,2%-%time:~0,2%:%time:~3,2%:%time:~6,2%
+echo %filename%
+git remote -v
+git add -A
+git status
+git commit -m %filename%
+git push orign computer_alienware
+pause
